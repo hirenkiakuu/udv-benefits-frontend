6d83b1a87466c43de8785e2a1a94037d694ed764
hirenkiaku <VladislavAndreev1337@yandex.ru>
2024-10-31 14:29:56 +0500

    feat: created filterpanel feature component

---
+++ udv-benefits-frontend/src/features/AvailabilityPanel/ui/AvailabilityPanel.tsx
@@ -0,0 +1,48 @@
+import cls from "./AvailabilityPanel.module.scss";
+import { classNames } from "shared/lib/classNames/classNames";
+import { Button } from "shared/ui";
+
+interface AvailabilityPanelProps {
+  className?: string;
+  currentBenefitsAvailability: string;
+  onAvailabilityChange: (currentBenefitsAvailability: string) => void;
+}
+
+const AvailabilityPanel = ({
+  className,
+  currentBenefitsAvailability,
+  onAvailabilityChange,
+}: AvailabilityPanelProps) => {
+  return (
+    <div className={classNames(cls.availabilityPanel, {}, [className])}>
+      <div className={cls.availabilityButtons}>
+        <Button
+          variant="text"
+          size="large"
+          isActive={currentBenefitsAvailability === "available"}
+          onClick={() => onAvailabilityChange("available")}
+        >
+          Доступные льготы
+        </Button>
+        <Button
+          variant="text"
+          size="large"
+          isActive={currentBenefitsAvailability === "purchased"}
+          onClick={() => onAvailabilityChange("purchased")}
+        >
+          Приобретенные льготы
+        </Button>
+        <Button
+          variant="text"
+          size="large"
+          isActive={currentBenefitsAvailability === "unavailable"}
+          onClick={() => onAvailabilityChange("unavailable")}
+        >
+          Недоступные льготы
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default AvailabilityPanel;
